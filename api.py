6524b5b6b91dd0f933034cdbc6a8f90062917c2e
root <root@li75-137.members.linode.com>
2014-02-28 14:34:42 -0500

    add celery

--- api.py
+++ api.py
@@ -10,9 +10,30 @@ from flask.ext.restful import reqparse
 from subprocess import call
 from datetime import datetime
 
+from celery import Celery
+
+def make_celery(app):
+    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
+
 app = Flask(__name__)
 
 api = restful.Api(app)
+
+app.config.update(
+    CELERY_BROKER_URL='redis://localhost:6379',
+    CELERY_RESULT_BACKEND='redis://localhost:6379'
+)
+celery = make_celery(app)
+
 
 @app.route("/")
 def main():
